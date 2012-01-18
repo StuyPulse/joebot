ba6ff9e456b8ad7cd489cb20feacc1c5c5497ac9
TentativelyOtherNoWangey <born2run5000@yahoo.com>
2012-01-18 16:34:55 -0500

    Added VictorSpeed.java.

---
+++ src/edu/stuy/speed/VictorSpeed.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.stuy.speed;
+
+/**
+ *
+ * @author admin
+ */
+public class VictorSpeed {
+    
+}
