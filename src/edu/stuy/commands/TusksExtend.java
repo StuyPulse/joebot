9aa99bb33e50634fc9592e9fc9c522e186d4f8b7
Danny Qiu <dqiu55@gmail.com>
2012-01-20 17:45:49 -0500

    Added Tusks Extend and Retract Commands

--- src/edu/stuy/commands/TusksExtend.java
+++ src/edu/stuy/commands/TusksExtend.java
@@ -8,45 +8,30 @@ package edu.stuy.commands;
  *
  * @author Danny
  */
-public class ConveyorConvey extends CommandBase {
-    boolean hasTimeout = false;
-    double timeout;
+public class TusksExtend extends CommandBase {
     
-    public ConveyorConvey() {
+    public TusksExtend() {
         // Use requires() here to declare subsystem dependencies
         // eg. requires(chassis);
-        requires(conveyor);
+        requires(tusks);
     }
-    
-    public ConveyorConvey(double timeout){
-        this();
-        hasTimeout = true;
-        this.timeout = timeout;
-    }
-    
+
     // Called just before this Command runs the first time
     protected void initialize() {
-        if (hasTimeout) {
-            setTimeout(timeout);
-        }
     }
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-        shooter.rollRollers(1, 1);
+        tusks.extend();
     }
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        if (hasTimeout) {
-            return isTimedOut();
-        }
         return false;
     }
 
     // Called once after isFinished returns true
     protected void end() {
-        shooter.rollRollers(0, 0);
     }
 
     // Called when another command which requires one or more of the same
