e488c85e62f050118236ccdad2be0ad312e6d8f4
Prog 694 <prog694@gmail.com>
2012-01-23 17:41:19 -0500

    Add refactored commands

---
+++ src/edu/stuy/commands/DrivetrainSetGear.java
@@ -0,0 +1,46 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.stuy.commands;
+
+import edu.stuy.commands.base.CommandBase;
+
+
+/**
+ *
+ * @author Prog
+ */
+public class DrivetrainSetGear extends CommandBase {
+    boolean high;
+
+    public DrivetrainSetGear(boolean high) {
+        // Use requires() here to declare subsystem dependencies
+        // eg. requires(chassis);
+        requires(drivetrain);
+        this.high = high;
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+        drivetrain.setGear(high);
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return false;
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    }
+}
---
+++ src/edu/stuy/commands/base/CommandBase.java
@@ -0,0 +1,47 @@
+package edu.stuy.commands.base;
+
+import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.stuy.OI;
+import edu.stuy.subsystems.*;
+
+/**
+ * The base for all commands. All atomic commands should subclass CommandBase.
+ * CommandBase stores creates and stores each control system. To access a
+ * subsystem elsewhere in your code in your code use CommandBase.exampleSubsystem
+ * @author Author
+ */
+public abstract class CommandBase extends Command {
+    public static OI oi;
+    
+    // Create a single static instance of all of your subsystems
+    public static Drivetrain drivetrain = new Drivetrain();
+    public static Shooter shooter = new Shooter();
+    public static Tusks tusks = new Tusks();
+    public static Acquirer acquirer = new Acquirer();
+    public static Conveyor conveyor = new Conveyor();
+
+    public static void init() {
+        // This MUST be here. If the OI creates Commands (which it very likely
+        // will), constructing it during the construction of CommandBase (from
+        // which commands extend), subsystems are not guaranteed to be
+        // yet. Thus, their requires() statements may grab null pointers. Bad
+        // news. Don't move it.
+        oi = new OI();
+
+        // Show what command your subsystem is running on the SmartDashboard
+        SmartDashboard.putData(drivetrain);
+        SmartDashboard.putData(shooter);
+        SmartDashboard.putData(tusks);
+        SmartDashboard.putData(acquirer);
+        SmartDashboard.putData(conveyor);
+    }
+
+    public CommandBase(String name) {
+        super(name);
+    }
+
+    public CommandBase() {
+        super();
+    }
+}
