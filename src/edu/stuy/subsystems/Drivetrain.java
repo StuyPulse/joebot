2818ed259ebedd03659b30b997751528e31588c3
Prog 694 <prog694@gmail.com>
2012-01-28 13:55:57 -0500

    Add PID drive straight controller to drivetrain subsystem

--- src/edu/stuy/subsystems/Drivetrain.java
+++ src/edu/stuy/subsystems/Drivetrain.java
@@ -10,9 +10,11 @@ import edu.stuy.commands.DriveManualJoystickControl;
 import edu.wpi.first.wpilibj.AnalogChannel;
 import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.Gyro;
+import edu.wpi.first.wpilibj.PIDOutput;
 import edu.wpi.first.wpilibj.RobotDrive;
 import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.smartdashboard.SendablePIDController;
 
 /**
  *
@@ -26,10 +28,17 @@ public class Drivetrain extends Subsystem {
     Encoder encoderLeft;
     Encoder encoderRight;
     Gyro gyro;
+    
+    SendablePIDController controller;
+
     final int WHEEL_RADIUS                 = 3; 
     final double CIRCUMFERENCE             = 2 * Math.PI * WHEEL_RADIUS;
     final int ENCODER_CODES_PER_REV        = 360;
     final double DISTANCE_PER_PULSE        = CIRCUMFERENCE / ENCODER_CODES_PER_REV;
+    
+    double Kp = 0.035;
+    double Ki = 0.0005;
+    double Kd = 1.0;
     
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
@@ -43,6 +52,14 @@ public class Drivetrain extends Subsystem {
         encoderRight.setDistancePerPulse(DISTANCE_PER_PULSE);
 
         gyro = new Gyro(RobotMap.GYRO_CHANNEL);
+        gyro.setSensitivity(0.007);
+        controller = new SendablePIDController(Kp, Ki, Kd, gyro, new PIDOutput() {
+
+            public void pidWrite(double output) {
+                drive.arcadeDrive(-1, -output);
+            }
+
+        }, 0.005);
 
         if (!Devmode.DEV_MODE) {
             gearShift = new Solenoid(RobotMap.GEAR_SHIFT);
@@ -63,5 +80,9 @@ public class Drivetrain extends Subsystem {
 
     public void setGear(boolean high) {
         gearShift.set(high);
+    }
+    
+    public void driveStraight(int distance) {
+        
     }
 }
