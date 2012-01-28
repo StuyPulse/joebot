09ab5047b463ea127d8ca79a56095e5a720acdc6
Prog 694 <prog694@gmail.com>
2012-01-28 11:17:07 -0500

    Add AutonSetting1

---
+++ src/edu/stuy/commands/AutonSetting1.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.stuy.commands;
+
+/**
+ *
+ * @author admin
+ */
+public class AutonSetting1 {
+    
+}
