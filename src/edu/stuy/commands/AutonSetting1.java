2fa595de436ec5f9327b07606a472c6b2cdc05c7
Alejandro Carrillo <aecarrillo594@gmail.com>
2012-01-29 04:01:48 -0500

    In AutonSetting1: remove trailing whitespace, add TODO

--- src/edu/stuy/commands/AutonSetting1.java
+++ src/edu/stuy/commands/AutonSetting1.java
@@ -20,16 +20,18 @@ public class AutonSetting1 extends CommandGroup {
 
     /* For inches to bridge from fender */
     public static final double INCHES_TO_BRIDGE = 5.0;
-    
+
     public AutonSetting1() {
         addSequential(new AutonDriveToFender(INCHES_TO_FENDER));
+
+        // TODO: Call ConveyAutomatic for a set time interval OR ConveySemiauto for two balls
         addSequential(new ShooterShoot(2.0));
-        
+
         // TODO: Get tusks running concurrently with backing up, have them retract after backuptobridge is done
         addParallel(new TusksExtend());
-        
+
         addSequential(new AutonBackUpToBridge(INCHES_TO_BRIDGE));
         addSequential(new TusksRetract());
-        
+
     }
 }
