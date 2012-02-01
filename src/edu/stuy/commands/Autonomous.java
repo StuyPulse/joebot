1d37bc6d07b1d4ee945b57df0e92808672a0cd49
Prog694 <captainpicardlam@gmail.com>
2012-02-01 17:06:52 -0500

    Made gears shift in Auton.

--- src/edu/stuy/commands/Autonomous.java
+++ src/edu/stuy/commands/Autonomous.java
@@ -32,23 +32,7 @@ public class Autonomous extends CommandGroup {
     public static double MAX_DIST_SPEED = 5;
 
     public Autonomous() {
-        // Add Commands here:
-        // e.g. addSequential(new Command1());
-        //      addSequential(new Command2());
-        // these will run in order.
-
-        // To run multiple commands at the same time,
-        // use addParallel()
-        // e.g. addParallel(new Command1());
-        //      addSequential(new Command2());
-        // Command1 and Command2 will run in parallel.
-
-        // A command group will require all of the subsystems that each member
-        // would require.
-        // e.g. if Command1 requires chassis, and Command2 requires arm,
-        // a CommandGroup containing them would require both the chassis and the
-        // arm.
-
+        addSequential(new DrivetrainSetGear(true));
 
     }
 }
