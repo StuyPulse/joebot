03c11f2fefe31290c1c632e61b06fd4d3d324594
TentativelyOtherNoWangey <born2run5000@yahoo.com>
2012-02-01 18:36:08 -0500

    Added sonar variable

--- src/edu/stuy/subsystems/Drivetrain.java
+++ src/edu/stuy/subsystems/Drivetrain.java
@@ -7,6 +7,7 @@ package edu.stuy.subsystems;
 import edu.stuy.Devmode;
 import edu.stuy.RobotMap;
 import edu.stuy.commands.DriveManualJoystickControl;
+import edu.wpi.first.wpilibj.AnalogChannel;
 import edu.wpi.first.wpilibj.RobotDrive;
 import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj.command.Subsystem;
@@ -20,6 +21,7 @@ public class Drivetrain extends Subsystem {
     RobotDrive drive;
     Solenoid gearShift;
     public Encoder leftEnc, rightEnc;
+    AnalogChannel sonar;
     
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
