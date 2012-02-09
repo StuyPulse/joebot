3d4ed0ce611f224fab421ffe0185dd830376a2d8
Kevin Wang <born2run5000@yahoo.com>
2012-02-09 16:13:16 -0500

    Add interrupted auton drive handling.

--- src/edu/stuy/commands/AutonDrive.java
+++ src/edu/stuy/commands/AutonDrive.java
@@ -46,5 +46,7 @@ public abstract class AutonDrive extends CommandBase {
     // Called when another command which requires one or more of the same
     // subsystems is scheduled to run
     protected void interrupted() {
+        System.out.println("interrupted");
+        end();
     }
 }
