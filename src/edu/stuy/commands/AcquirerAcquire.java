00d540cd288688bf59be0da1814c77e5377c0c5a
Q <zqiu1994@gmail.com>
2012-02-12 13:28:00 -0500

    Stop conveyor when acquirer command stops.

--- src/edu/stuy/commands/AcquirerAcquire.java
+++ src/edu/stuy/commands/AcquirerAcquire.java
@@ -66,6 +66,7 @@ public class AcquirerAcquire extends CommandBase {
     // Called once after isFinished returns true
     protected void end() {
         acquirer.stop();
+        conveyor.stop();
     }
 
     // Called when another command which requires one or more of the same
