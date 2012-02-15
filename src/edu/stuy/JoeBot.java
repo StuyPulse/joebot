873ad4f71ba73959d3a7810877fd6dba56169ec1
Kevin Wang <born2run5000@yahoo.com>
2012-02-15 18:58:27 -0500

    comment out camera

--- src/edu/stuy/JoeBot.java
+++ src/edu/stuy/JoeBot.java
@@ -44,9 +44,6 @@ public class JoeBot extends IterativeRobot {
         // Initialize all subsystems
         CommandBase.init();
         
-        //CameraVision.getInstance();
-        //CameraVision.getInstance().doCamera();
-        //CameraVision.getInstance().toggleTargetLightIfAligned();
     }
     
     public void disabledPeriodic() {
