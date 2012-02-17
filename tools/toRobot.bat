fd31eb6d27549336fed187aeb16d83025415efa2
Q <zqiu1994@gmail.com>
2012-02-17 18:52:31 -0500

    now it askes for the last number of the ipaddress when connecting to bot

--- tools/toRobot.bat
+++ tools/toRobot.bat
@@ -2,7 +2,7 @@
 @ECHO This fanciness is brought to you by Folders!
 @ECHO You may have to change the text in "Wireless Network Connection"
 
-@REM set /P lastnumberofip=ENTER_LAST_DIGIT_OF_IP_YOU_WANT_TO_USE:
+@set /P lastnumberofip=ENTER_LAST_DIGIT_OF_IP_YOU_WANT_TO_USE:
 
 @set varip=10.6.94.%lastnumberofip%
 @ECHO ip address %varip%
