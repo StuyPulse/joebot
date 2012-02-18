81982426224bd07cc6a1f282683b864202afe35c
Alejandro (Alex) Carrillo <aecarrillo594@gmail.com>
2012-02-18 13:40:21 -0500

    Remove OI test code.

--- src/edu/stuy/JoeBot.java
+++ src/edu/stuy/JoeBot.java
@@ -99,13 +99,6 @@ public class JoeBot extends IterativeRobot {
         // Debug box actions
         CommandBase.oi.updateLights();
         updateSmartDashboard();
-        
-        while(CommandBase.oi.getLeftStick().getRawButton(10)){
-            CommandBase.oi.turnOnLights();
-        }
-        if(CommandBase.oi.getLeftStick().getRawButton(11)){
-            CommandBase.oi.turnOffLights();
-        }
     }
     
     // We use SmartDashboard to monitor bot information.
