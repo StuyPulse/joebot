95a8dcc22b9db7cda66e3676de819306f6fe4116
Prog 694 <prog694@gmail.com>
2012-02-18 11:15:46 -0500

    Finished refactoring Auton Settings to reflect OI numbers.

---
+++ src/edu/stuy/commands/AutonSetting0.java
@@ -0,0 +1,29 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.stuy.commands;
+
+import edu.stuy.subsystems.Flywheel;
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+/**
+ *
+ * @author admin
+ */
+public class AutonSetting0 extends CommandGroup {
+
+    public AutonSetting0() {
+        addSequential(new AutonDriveToFender(Autonomous.INCHES_TO_FENDER));
+
+        double distanceInches = Flywheel.distances[Flywheel.FENDER_INDEX];
+        addSequential(new FlywheelRun(distanceInches, Flywheel.speedsTopHoop));
+        addSequential(new ConveyAutomatic(4)); //4 sec is a raw value. Change.
+
+        // TODO: Get tusks running concurrently with backing up, have them retract after backuptobridge is done
+        addParallel(new TusksExtend());
+        addSequential(new AutonBackUpToBridge(Autonomous.INCHES_TO_BRIDGE));
+        addSequential(new TusksRetract());
+
+    }
+}
