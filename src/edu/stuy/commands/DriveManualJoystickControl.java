38dc757b4284b50549b86fbac5e7f5a91f467bd7
Yulli Equine <yulli@yulli.org>
2012-03-13 17:37:28 -0400

    Remove compressor disablement while driving

--- src/edu/stuy/commands/DriveManualJoystickControl.java
+++ src/edu/stuy/commands/DriveManualJoystickControl.java
@@ -29,11 +29,6 @@ public class DriveManualJoystickControl extends CommandBase {
     protected void execute() {
         drivetrain.tankDrive(-oi.getLeftStick().getY(), -oi.getRightStick().getY());
         // If we're driving, stop the compressor from turning on. Otherwise, it's fine.
-        if ( Math.abs(oi.getLeftStick().getY()) < 0.1  || Math.abs(oi.getRightStick().getY()) < 0.1) {
-            drivetrain.compressor.stop();
-        } else {
-            drivetrain.compressor.start();
-        }
     }
 
     // Make this return true when this Command no longer needs to run execute()
