257258dedad5b01e126d2c82569aa5b33b17bb81
Yulli Equine <yulli@yulli.org>
2012-04-05 17:27:12 -0400

    re-add CameraVision.java

--- src/edu/stuy/camera/CameraVision.java
+++ src/edu/stuy/camera/CameraVision.java
@@ -31,21 +31,19 @@ import java.util.Vector;
  * sample images as well as the NI Vision Assistant file that contains the
  * vision command chain (open it with the Vision Assistant)
  */
-public class CameraVision {
+public class CameraVision extends Thread {
 
-    private static CameraVision instance;
-    //TODO get number from Peter
-    public static final int CAMERA_CENTER = 320;
+    private static CameraVision instance;   // CameraVision is a singleton
     AxisCamera camera;          // the axis camera object (connected to the switch)
     CriteriaCollection cc;      // the criteria for doing the particle filter operation
-    private Relay targetLight;
-    private Relay reflectiveLight;
-    private int targetCenter;
-    Vector distances = new Vector();
-    int numRectangles;
-    Vector massCenter = new Vector();
+    private Relay targetLight;              // Are-we-aligned? indicator
+    private Relay reflectiveLight;          // To make the targets luminous
+    private int targetCenter;               // x-coord of the particle center-of-mass
+    int CAMERA_CENTER;                      // center of camera width
+    Vector massCenter = new Vector();       // list of center-of-mass-es
+    boolean cameraOn;
 
-    public static CameraVision getInstance() {
+    public static CameraVision getInstance() {  // CameraVision is a singleton
         if (instance == null) {
             instance = new CameraVision();
         }
@@ -59,101 +57,72 @@ public class CameraVision {
         reflectiveLight = new Relay(RobotMap.REFLECTIVE_LIGHT);
         reflectiveLight.setDirection(Relay.Direction.kForward);
 
-        camera = AxisCamera.getInstance();  // get an instance ofthe camera
+        camera = AxisCamera.getInstance();  // get an instance of the camera
+        CAMERA_CENTER = camera.getResolution().width / 2;
         cc = new CriteriaCollection();      // create the criteria for the particle filter
-        cc.addCriteria(MeasurementType.IMAQ_MT_BOUNDING_RECT_WIDTH, 30, 400, false);
-        cc.addCriteria(MeasurementType.IMAQ_MT_BOUNDING_RECT_HEIGHT, 40, 400, false);
-        
+        cc.addCriteria(MeasurementType.IMAQ_MT_BOUNDING_RECT_WIDTH, 30, 240, false);  // any particles at least 30 pixels wide
+        cc.addCriteria(MeasurementType.IMAQ_MT_BOUNDING_RECT_HEIGHT, 40, 320, false); // any particles at least 40 pixels high
+
     }
 
     public void doCamera() {
-        if (toggleReflectLightIfInRange()) {
+        while(cameraOn){
+        if (toggleReflectLightIfInRange()) { // if we're within image-accurate distance
             try {
                  // Do the image capture with the camera and apply the algorithm described above.
-                ColorImage image = camera.getImage();
-                BinaryImage rectImage = image.thresholdHSL(145, 182, 0, 255, 140, 255);
-                //rectImage.write("red.png");
-
-
-                BinaryImage bigObjectsImage = rectImage.removeSmallObjects(false,2);  // remove small artifacts
-                for(int x = 0; x<5;x++){
-                BinaryImage bigObjectsImage2 = bigObjectsImage.removeSmallObjects(false,2);
-                bigObjectsImage.free();
-                bigObjectsImage = bigObjectsImage2.removeSmallObjects(false,2);
-                bigObjectsImage2.free();
-                }
-
-                BinaryImage convexHullImage = bigObjectsImage.convexHull(false);          // fill in occluded rectangles
-                //convexHullImage.write("box.png");
-                BinaryImage filteredImage = convexHullImage.particleFilter(cc);           // find filled in rectangles
-                filteredImage.write("rawr.png");
-                ParticleAnalysisReport[] reports = filteredImage.getOrderedParticleAnalysisReports();  // get list of results
-                massCenter.removeAllElements();
-                distances.removeAllElements();
-                for (int i = 0; i < reports.length; i++) {                                // print results
-                    ParticleAnalysisReport r = reports[i];
-                    // [target size feet]/[target size pixels] = [FOV feet]/[FOV pixels]
-                    // [FOV feet] = ([FOV pixels]*[target size feet])/[target size pixels]
-                    int fovFeet = (360 * 2) / r.boundingRectWidth;
-                    distances.addElement(new Double((fovFeet / 2) / 2.1348966977217008));
-                    massCenter.addElement(new Integer(r.center_mass_x));
-                    //System.out.println("Our field of view in feet is: " + fovFeet);
-                    //System.out.println("Particle: " + i + ":  Center of mass x: " + r.center_mass_x);
-                    // Calculate distance using tan(theta) = [half-width of target]/[distance to target]
-                    // [distance to target] = [half-width of target]/tan(theta)
-
-                    //System.out.println("Distance to target: " + ((fovFeet / 2) / 2.1348966977217008));
-                }
-//                System.out.println(filteredImage.getNumberParticles() + "  " + Timer.getFPGATimestamp());
-                if (massCenter.size() > 0) {
-                    targetCenter = getCenterMass(0);
-                }
-
-                /**
-                 * all images in Java must be freed after they are used since
-                 * they are allocated out of C data structures. Not calling
-                 * free() will cause the memory to accumulate over each pass of
-                 * this loop.
-                 */
-                filteredImage.free();
-                convexHullImage.free();
-                bigObjectsImage.free();
+                ColorImage image = camera.getImage(); // get the image from the camera
+                BinaryImage rectImage = image.thresholdHSL(145, 182, 0, 255, 120, 255); // mark only areas that have high
+                image.free();                                                                        // luminance (the last two numbers
+                                                                                        // are low-high limits
+                BinaryImage bigObjectsImage = rectImage.removeSmallObjects(false, 2);  // remove small artifacts
                 rectImage.free();
-                image.free();
+                BinaryImage convexHullImage = bigObjectsImage.convexHull(false);          // fill in occluded rectangles
+                bigObjectsImage.free();
+                BinaryImage filteredImage = convexHullImage.particleFilter(cc);           // filter particles using our criteria
+                convexHullImage.free();
+                ParticleAnalysisReport[] reports = filteredImage.getOrderedParticleAnalysisReports();  // get list of results
+                massCenter.removeAllElements();                                         // remove previous center-of-mass-es
+                
+                
+                ParticleAnalysisReport r = reports[0]; // Gets largest detected target
+                
+                // [target size feet]/[target size pixels] = [FOV feet]/[FOV pixels]
+                // [FOV feet] = ([FOV pixels]*[target size feet])/[target size pixels]
+                int fovFeet = (360 * 2) / r.boundingRectWidth;                          // use it to calculate our field of view
+                massCenter.addElement(new Integer(r.center_mass_x));                    // add each center-of-mass to our list
 
+
+                if (massCenter.size() > 0) {            // if there are center-of-mass-es in the list
+                    targetCenter = getCenterMass(0);    // set targetCenter to the largest one (the first one)
+                }
+
+                filteredImage.free();
+               
             } catch (AxisCameraException ex) {          // this is needed if the camera.getImage() is called
-                ex.printStackTrace();
             } catch (NIVisionException ex) {
-                ex.printStackTrace();
             }
         }
     }
-
+    }
     /**
      * Displays the center of the largest, rectangular target detected
      * @return targetCenter
      */
     public double getTargetCenter() {
-        return targetCenter;
-    }
-
-    public double getDistance(int rectid) {
-        if (rectid < distances.size()) {
-            return ((Double) distances.elementAt(rectid)).doubleValue();
-        }
-        return 0.0;
+        return targetCenter;    // returns the largest center-of-mass
     }
 
     public int getCenterMass(int rectid) {
         if (rectid < massCenter.size()) {
-            return ((Integer) massCenter.elementAt(rectid)).intValue();
+            return ((Integer) massCenter.elementAt(rectid)).intValue(); // return the center-of-mass for the specified rectangle
         }
-        return 0;
+        return -1; // but if trying to find a non-existent rectangle, return nonsense
     }
 
     public boolean isAligned() {
         double absValue = Math.abs(CAMERA_CENTER - targetCenter);
-        return absValue < 50;
+        return absValue < 20; // if the pixel difference between the center-of-mass
+                              // and the center-of-camera, return True
 
     }
 
@@ -174,7 +143,23 @@ public class CameraVision {
      */
     public void toggleTargetLightIfAligned() {
         targetLight.set(isAligned() ? Relay.Value.kOn : Relay.Value.kOff);
+    }
 
+    public void run(){
+        doCamera();
+        toggleTargetLightIfAligned();
+        toggleReflectLightIfInRange();
+    }
 
+    public void setCamera(boolean  isRunning){
+        cameraOn = isRunning;
+    }
+    
+    public void servoUp() {
+        // move the servo UP
+    }
+    
+    public void servoDown() {
+        // move the servo DOWN
     }
 }
